3aa9ce950f88e61b285e8281276e9e0ce1eec018
shafiquddin <shafiquddin2k@gmail.com>
2024-07-02 01:03:26 +0530

    Authentication

--- frontend/src/pages/Authentication.js
+++ frontend/src/pages/Authentication.js
@@ -1,4 +1,4 @@
-import { json } from "react-router-dom";
+import { json, redirect } from "react-router-dom";
 import AuthForm from "../components/AuthForm";
 
 function AuthenticationPage() {
@@ -7,20 +7,28 @@ function AuthenticationPage() {
 
 export default AuthenticationPage;
 
-// export const actions = async ({ request, params }) => {
-//   const data = await request.formData();
-//   const crendentials = Object.fromEntries(data.entries());
-//   const response = await fetch("http://localhost:8080/auth", {
-//     method: request.method,
-//     body: JSON.stringify(crendentials),
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//   });
-//   if(!response.ok){
-//     throw json({message:'Could not send request'},{status:500})
-//   }else{
-//     const resData = await response.json();
-//     console.log(resData);
-//   }
-// };
+export const actions = async ({ request, params }) => {
+  const data = await request.formData();
+  const crendentials = Object.fromEntries(data.entries());
+  const searchParams = new URL(request.url).searchParams;
+  const mode = searchParams.get('mode') || 'login'
+  if(mode !== 'login' && mode !== 'signup'){
+    throw json({message:'Unsupported Mode!'},{status:422})
+  }
+  const response = await fetch(`http://localhost:8080/${mode}`, {
+    method: request.method,
+    body: JSON.stringify(crendentials),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if(response.status === 422 || response.status === 401){
+    return response;
+  }
+
+  if(!response.ok){
+    throw json({message:'Could not send request'},{status:500})
+  }
+  return redirect('/');
+};
